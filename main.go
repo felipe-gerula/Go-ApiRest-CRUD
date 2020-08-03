fb050df2618f39487e2854d49a3205c44828a448
felipe-gerula <felipe.gerula@gmail.com>
2020-08-03 17:01:51 -0300

    creacion de peticion DELETE

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import(
 	"log"
 	"encoding/json"
 	"io/ioutil"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -49,6 +50,38 @@ func createTasks(w http.ResponseWriter , r *http.Request){
 	json.NewEncoder(w).Encode(newTask)
 }
 
+func getTask(w http.ResponseWriter , r *http.Request){
+	vars := mux.Vars(r)
+	taskID , err := strconv.Atoi(vars["id"])
+	if err != nil{
+		fmt.Fprintf(w,"Invalid ID")
+		return 
+	}
+
+	for _ , task := range tasks{
+		if task.ID == taskID{
+			w.Header().Set("Content-Type" , "application/json")
+			json.NewEncoder(w).Encode(task)
+		}
+	}
+}
+
+func deleteTask(w http.ResponseWriter , r *http.Request){
+	vars := mux.Vars(r)
+	taskID , err := strconv.Atoi(vars["id"])
+	if err != nil{
+		fmt.Fprintf(w,"Invalid ID")
+		return
+	}
+	for i,task := range tasks{
+		if task.ID == taskID{
+			tasks = append(tasks[:i] , tasks[i + 1:]...)
+			fmt.Fprintf(w,"the task with ID %v has been deleted succesfully", taskID)
+		}
+	}
+
+}
+
 func indexRoute(w http.ResponseWriter , r *http.Request){
 	fmt.Fprintf(w,"welcome to my API")
 }
@@ -59,6 +92,8 @@ func main(){
 	router.HandleFunc("/",indexRoute)
 	router.HandleFunc("/tasks",getTasks).Methods("GET")
 	router.HandleFunc("/tasks",createTasks).Methods("POST")
+	router.HandleFunc("/tasks/{id}",getTask).Methods("GET")
+	router.HandleFunc("/tasks/{id}",deleteTask).Methods("DELETE")
 
 	log.Fatal(http.ListenAndServe(":3000",router)) 
 }
